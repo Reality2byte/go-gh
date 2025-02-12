897a7fe80379d001a7ed4c4e392442e72b6fc189
Tyler McGoffin <jtmcg@github.com>
2025-02-11 16:02:49 -0800

    Add accessible styles

--- pkg/markdown/accessibility.go
+++ pkg/markdown/accessibility.go
@@ -2,6 +2,9 @@ package markdown
 
 import (
 	"strconv"
+
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/glamour/ansi"
 )
 
 type AnsiColor int
@@ -52,6 +55,73 @@ func (a AnsiColor) Value() string {
 
 func (a AnsiColor) ValuePtr() *string {
 	return strPtr(a.Value())
+}
+
+func AccessibleStyleConfig(theme string) ansi.StyleConfig {
+	switch theme {
+	case "light":
+		return accessibleLightStyleConfig()
+	case "dark":
+		return accessibleDarkStyleConfig()
+	default:
+		return ansi.StyleConfig{}
+	}
+}
+
+func accessibleDarkStyleConfig() ansi.StyleConfig {
+	cfg := glamour.DarkStyleConfig
+
+	// Text color
+	cfg.Document.StylePrimitive.Color = White.ValuePtr()
+
+	// Link colors
+	cfg.Link.Color = Black.ValuePtr()
+	cfg.LinkText.Color = BrightCyan.ValuePtr()
+
+	// Heading colors
+	cfg.Heading.StylePrimitive.Color = BrightMagenta.ValuePtr()
+	cfg.H1.StylePrimitive.Color = BrightWhite.ValuePtr()
+	cfg.H1.StylePrimitive.BackgroundColor = BrightBlue.ValuePtr()
+
+	// Code colors
+	cfg.Code.BackgroundColor = BrightWhite.ValuePtr()
+	cfg.Code.Color = Red.ValuePtr()
+
+	// Image colors
+	cfg.Image.Color = BrightMagenta.ValuePtr()
+
+	// Horizontal rule colors
+	cfg.HorizontalRule.Color = White.ValuePtr()
+
+	return cfg
+}
+
+func accessibleLightStyleConfig() ansi.StyleConfig {
+	cfg := glamour.LightStyleConfig
+
+	// Text color
+	cfg.Document.StylePrimitive.Color = Black.ValuePtr()
+
+	// Link colors
+	cfg.Link.Color = Black.ValuePtr()
+	cfg.LinkText.Color = BrightBlue.ValuePtr()
+
+	// Heading colors
+	cfg.Heading.StylePrimitive.Color = Magenta.ValuePtr()
+	cfg.H1.StylePrimitive.Color = BrightWhite.ValuePtr()
+	cfg.H1.StylePrimitive.BackgroundColor = Blue.ValuePtr()
+
+	// Code colors
+	cfg.Code.BackgroundColor = BrightWhite.ValuePtr()
+	cfg.Code.Color = Red.ValuePtr()
+
+	// Image colors
+	cfg.Image.Color = Magenta.ValuePtr()
+
+	// Horizontal rule colors
+	cfg.HorizontalRule.Color = White.ValuePtr()
+
+	return cfg
 }
 
 func strPtr(s string) *string {
--- pkg/markdown/accessibility_test.go
+++ pkg/markdown/accessibility_test.go
@@ -3,6 +3,8 @@ package markdown
 import (
 	"testing"
 
+	"github.com/charmbracelet/glamour"
+	"github.com/charmbracelet/glamour/ansi"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -76,4 +78,67 @@ func TestAnsiColorValuePtr(t *testing.T) {
 			assert.Equal(t, tt.want, tt.c.ValuePtr())
 		})
 	}
+}
+
+func TestAccessibleStyleConfig(t *testing.T) {
+	tests := []struct {
+		name  string
+		theme string
+		want  ansi.StyleConfig
+	}{
+		{
+			name:  "light",
+			theme: "light",
+			want:  accessibleLightStyleConfig(),
+		},
+		{
+			name:  "dark",
+			theme: "dark",
+			want:  accessibleDarkStyleConfig(),
+		},
+		{
+			name:  "fallback",
+			theme: "foo",
+			want:  ansi.StyleConfig{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.want, AccessibleStyleConfig(tt.theme))
+		})
+	}
+}
+
+func Test_accessibleDarkStyleConfig(t *testing.T) {
+	cfg := accessibleDarkStyleConfig()
+	assert.Equal(t, White.ValuePtr(), cfg.Document.StylePrimitive.Color)
+	assert.Equal(t, Black.ValuePtr(), cfg.Link.Color)
+	assert.Equal(t, BrightCyan.ValuePtr(), cfg.LinkText.Color)
+	assert.Equal(t, BrightMagenta.ValuePtr(), cfg.Heading.StylePrimitive.Color)
+	assert.Equal(t, BrightWhite.ValuePtr(), cfg.H1.StylePrimitive.Color)
+	assert.Equal(t, BrightBlue.ValuePtr(), cfg.H1.StylePrimitive.BackgroundColor)
+	assert.Equal(t, BrightWhite.ValuePtr(), cfg.Code.BackgroundColor)
+	assert.Equal(t, Red.ValuePtr(), cfg.Code.Color)
+	assert.Equal(t, BrightMagenta.ValuePtr(), cfg.Image.Color)
+	assert.Equal(t, White.ValuePtr(), cfg.HorizontalRule.Color)
+
+	// Test that we haven't changed the original style
+	assert.Equal(t, glamour.LightStyleConfig.H2, cfg.H2)
+}
+
+func Test_accessibleLightStyleConfig(t *testing.T) {
+	cfg := accessibleLightStyleConfig()
+	assert.Equal(t, Black.ValuePtr(), cfg.Document.StylePrimitive.Color)
+	assert.Equal(t, Black.ValuePtr(), cfg.Link.Color)
+	assert.Equal(t, BrightBlue.ValuePtr(), cfg.LinkText.Color)
+	assert.Equal(t, Magenta.ValuePtr(), cfg.Heading.StylePrimitive.Color)
+	assert.Equal(t, BrightWhite.ValuePtr(), cfg.H1.StylePrimitive.Color)
+	assert.Equal(t, Blue.ValuePtr(), cfg.H1.StylePrimitive.BackgroundColor)
+	assert.Equal(t, BrightWhite.ValuePtr(), cfg.Code.BackgroundColor)
+	assert.Equal(t, Red.ValuePtr(), cfg.Code.Color)
+	assert.Equal(t, Magenta.ValuePtr(), cfg.Image.Color)
+	assert.Equal(t, White.ValuePtr(), cfg.HorizontalRule.Color)
+
+	// Test that we haven't changed the original style
+	assert.Equal(t, glamour.LightStyleConfig.H2, cfg.H2)
 }
