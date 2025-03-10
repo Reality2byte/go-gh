e9d3643828faa9a2781f7c8fd6e93907d60c4004
Andy Feller <andyfeller@github.com>
2025-03-10 10:10:50 -0400

    Introduce experimental accessibile markdown colors
    Relates github/cli#830

---
+++ pkg/x/color/color.go
@@ -0,0 +1,2 @@
+// Package color handles common GitHub CLI user experiences focused on color rendering concerns such as accessibility and color roles.
+package color
--- pkg/x/x.go
+++ pkg/x/x.go
@@ -1,4 +1,4 @@
-// `x` is a collection of experimental features being used within the GitHub CLI following common Go conventions.
+// Package x is a collection of experimental features for use within the GitHub CLI and CLI extensions.
 //
 // Anything contained is subject to change without notice until it is considered stable enough to be promoted.
 package x
